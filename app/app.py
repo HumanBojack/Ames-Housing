05dddc9006d6b64805cb1f2fbb0685f97d74f416
HumanBojack <spychalar59@gmail.com>
2022-11-07 13:34:29 +0100

    feat: adapt page to handle the new model with different columns

--- app/app.py
+++ app/app.py
@@ -3,28 +3,33 @@ import numpy as np
 import pandas as pd
 import joblib
 
+NEIGHBORHOODS = ['NAmes', 'Gilbert', 'StoneBr', 'NWAmes', 'Somerst', 'BrDale',
+       'NPkVill', 'NridgHt', 'Blmngtn', 'NoRidge', 'SawyerW', 'Sawyer',
+       'Greens', 'BrkSide', 'OldTown', 'IDOTRR', 'ClearCr', 'SWISU',
+       'Edwards', 'CollgCr', 'Crawfor', 'Blueste', 'Mitchel', 'Timber',
+       'MeadowV', 'Veenker', 'GrnHill', 'Landmrk']
+
 app = Flask(__name__)
 model = joblib.load(open('model.joblib', 'rb'))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', neighborhoods=NEIGHBORHOODS)
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     X_predict = {}
-    for var in ['Year_Built', 'Total_Bsmt_SF', '1st_Flr_SF', 'Gr_Liv_Area','Garage_Area', 'Overall_Qual', 'Full_Bath', 'Exter_Qual',
-              'Kitchen_Qual', 'Neighborhood']:
-        if var in ['Exter_Qual','Kitchen_Qual', 'Neighborhood']:
+    for var in ['BsmtFin SF 1', 'Gr Liv Area', 'House Area', 'Neighborhood', 'Fireplaces', 'Overall Qual', 'Bsmt Qual', 'Kitchen Qual', 'Year Built', 'Year Remod/Add']:
+        if var in ["Neighborhood", "Bsmt Qual", "Kitchen Qual"]:
             X_predict[var]= request.form[var]
         else:
             X_predict[var]= int(request.form[var])
 
     pred = model.predict(pd.DataFrame(X_predict, index=[0]))
 
-    return render_template('index.html', data=int(pred))
+    return render_template('index.html', data=int(pred), neighborhoods=NEIGHBORHOODS)
 
 
 if __name__ == '__main__':
