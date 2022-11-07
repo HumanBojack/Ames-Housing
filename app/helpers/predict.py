c1e6b0a44c1164a7f8b303d38ebcd55112308e65
HumanBojack <spychalar59@gmail.com>
2022-11-07 14:55:09 +0100

    refactor: remove numpy import

--- app/helpers/predict.py
+++ app/helpers/predict.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def parse_predict_form(form):
     X_predict = {}
     for var in ['BsmtFin SF 1', 'Gr Liv Area', 'House Area', 'Neighborhood', 'Fireplaces', 'Overall Qual', 'Bsmt Qual', 'Kitchen Qual', 'Year Built', 'Year Remod/Add']:
